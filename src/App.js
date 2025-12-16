f5f20651073a4a36b627f7f8784715cfcfe15db7
QoderAI <215938697+qoderai[bot]@users.noreply.github.com>
2025-12-16 12:15:05 +0000

    feat(todo): add todo list functionality
    Co-authored-by: Qianyu Yan <>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PersonnelManagementPage from './pages/PersonnelManagementPage';
+import TodoListPage from './pages/TodoListPage';
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route path="/personnel-management" component={PersonnelManagementPage} />
+        <Route path="/todo" component={TodoListPage} />
       </Switch>
     </Router>
   );
